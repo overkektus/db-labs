cdbbb0befc21689a1f212af6223279f67248b5f5
Egor Piskunou <egor.piskunou@gmail.com>
2017-11-12 19:34:26 +0300

    add lab10 Stetho dependency

--- Lab10/SQLite2/app/src/main/java/ped/bstu/by/sqlite2/MainActivity.java
+++ Lab10/SQLite2/app/src/main/java/ped/bstu/by/sqlite2/MainActivity.java
@@ -2,6 +2,9 @@ package ped.bstu.by.sqlite2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ResourceCursorAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-}
+
+    private void initialStetho() {
+        // Create an InitializerBuilder
+        Stetho.InitializerBuilder initializerBuilder =
+                Stetho.newInitializerBuilder(this);
+
+        // Enable Chrome DevTools
+        initializerBuilder.enableWebKitInspector(
+                Stetho.defaultInspectorModulesProvider(this)
+        );
+
+        // Enable command line interface
+        initializerBuilder.enableDumpapp(
+                Stetho.defaultDumperPluginsProvider(this)
+        );
+
+        // Use the InitializerBuilder to generate an Initializer
+        Stetho.Initializer initializer = initializerBuilder.build();
+
+        // Initialize Stetho with the Initializer
+        Stetho.initialize(initializer);
+    }}
